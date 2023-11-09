dc17c20374e35b843b61a20800c2eb641b75a30d
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2023-11-09 19:06:56 +0100

    updated

--- src/ejercicio10.py
+++ src/ejercicio10.py
@@ -12,12 +12,77 @@
     Mostrar lista de todos los clientes de la base datos con su NIF y nombre.
     Mostrar la lista de clientes preferentes de la base de datos con su NIF y nombre.
     Terminar el programa.'''
-
-
-
-if __name__ =="__main__":
-    #Entrada
     
-    #Proceso
+if __name__ =="__main__": 
+    clientes = {}
+    continuar = "1"
     
-    #Salida
+    while continuar == "1":
+        
+        print("\n- Menú de opciones:")
+        print("\n\t1. Añadir cliente")
+        print("\t2. Eliminar cliente")
+        print("\t3. Mostrar cliente")
+        print("\t4. Listar todos los clientes")
+        print("\t5. Listar clientes preferentes ")
+        print("\t6. Terminar")
+        
+        opcion = str(input("\nSeleccione una opción (1/2/3/4/5/6): "))
+
+        if opcion == "1":
+            nif = input("Introduzca el NIF del cliente: ")
+            nombre = input("Introduzca el nombre del cliente: ")
+            direccion = input("Introduzca la dirección del cliente: ")
+            telefono = input("Introduzca el telefono del cliente: ")
+            correo = input("Introduzca el correo del cliente: ")
+            preferente = input("¿Es cliente preferente? \n\t(1)--> Sí. \n\t(2)--> no")
+            
+            cliente = {'nombre':nombre, 'direccion':direccion, 'telefono':telefono, 'correo':correo, 'preferente':preferente}
+            clientes[nif] = cliente
+            
+            continuar = input("¿Desea continuar? \n\t(1)--> Sí. \n\t(2)--> No.")
+            
+        elif opcion == "2":
+            nif = input("Introduzca el nif del cliente que desea eliminar: ")
+            if nif in clientes:
+                del clientes[nif]
+                mensaje = "Cliente con nif " + str(nif) + " ha sido eliminado"
+                continuar = input("¿Desea continuar? \n\t(1)--> Sí. \n\t(2)--> No.")
+                
+            else:
+                mensaje = "El cliente con nif " + str(nif) + " no se encuentra en la base de datos"
+                continuar = input("¿Desea continuar? \n\t(1)--> Sí. \n\t(2)--> No.")
+        
+        elif opcion == "3":
+            nif = input("Introduzca el nif del cliente que quiera mostrar: ")
+            if nif in clientes:
+                cliente = clientes[nif]
+                print("Datos del cliente con NIF " + nif + ":")
+                for clave, valor in cliente.items():
+                    print(clave + ": " + str(valor))
+            else:
+                mensaje = "El cliente con NIF " + nif + " no se encuentra en la base de datos"
+            
+            continuar = input("¿Desea continuar? \n\t(1)--> Sí. \n\t(2)--> No.")
+            
+            
+        elif opcion == "4":
+            claves = clientes.values()
+            print(list(claves))
+            
+            continuar = input("¿Desea continuar? \n\t(1)--> Sí. \n\t(2)--> No.")
+        
+        elif opcion == "5":
+            for nif, cliente in clientes.items():
+                if cliente['preferente'] == "1":
+                    print(nif, cliente)
+                    
+                    
+            continuar = input("¿Desea continuar? \n\t(1)--> Sí. \n\t(2)--> No.")
+            
+
+        elif opcion == "6":
+            print("Fin del programa.")
+            continuar = 0
+                
+            
--- src/ejercicio11.py
+++ src/ejercicio11.py
@@ -1,18 +1,14 @@
 ''' El directorio de los clientes de una empresa está organizado en una cadena de texto como la de más 
     abajo, donde cada línea contiene la información del nombre, email, teléfono, nif, y el descuento que 
-    se le aplica. Las líneas se separan con el carácter de cambio de línea \n y la primera línea contiene 
+    se le aplica. Las líneas se separan con el carácter de cambio de línea y la primera línea contiene 
     los nombres de los campos con la información contenida en el directorio.
     
     Escribir un programa que genere un diccionario con la información del directorio, donde cada elemento
     corresponda a un cliente y tenga por clave su nif y por valor otro diccionario con el resto de la información 
     del cliente. Los diccionarios con la información de cada cliente tendrán como claves los nombres de los campos 
-    y como valores la información de cada cliente correspondientes a los campos.'''
+    y como valores la información de cada cliente correspondientes a los campos. '''
 
 
 
 if __name__ =="__main__":
-    #Entrada
     
-    #Proceso
-    
-    #Salida
